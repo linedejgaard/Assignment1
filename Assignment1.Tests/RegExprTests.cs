ae67c4a5922c1554ea88dac2c5c83d4dec036c31
jard <jard@itu.dk>
2021-09-13 16:42:37 +0200

    Implement resolution

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -4,5 +4,15 @@ namespace Assignment1.Tests
 {
     public class RegExprTests
     {
+        [[Fact]
+        public void Resolution_given_list_of_8_resolutions_returns_correct_format()
+        {
+        //Given
+        
+        
+        //When
+        
+        //Then
+        }]
     }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Assignment1
 {
@@ -7,12 +8,21 @@ namespace Assignment1
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
         }
 
         public static IEnumerable<(int width, int height)> Resolution(string resolutions)
         {
-            throw new NotImplementedException();
+            
+            foreach (var resolution in resolutions)
+            {
+                MacthCollection matches = Regex.Match("(?P<witdh>[0-9]+)+x(?P<height>[0-9]+)");
+                foreach (Macth resolution in matches)
+                {
+                    var stitch = "(" + resolution.Groups["width"].value + "," + resolution.Groups["height"] +")"
+                    yield stitch;
+                }
+
+            }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
