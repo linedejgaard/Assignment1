bbc5d0fc48520b6759ae00805df093c837b544ca
jard <jard@itu.dk>
2021-09-15 16:49:32 +0200

    Implement test of Resolution that passes

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -7,25 +7,20 @@ namespace Assignment1.Tests
     public class RegExprTests
     {
         [Fact]
-        public void Resolution_given_list_of_8_resolutions_returns_correct_format()
+        public void Resolution_given_list_of_4_resolutions_returns_8_int_width_pairs()
         {
         List<string> resolutions = new List<string>();
-        resolution.Add("1920x1080");
-        resolution.Add("1024x768, 800x600, 640x480");
-        resolution.Add("320x200, 320x240, 800x600");
-        resolution.Add("1280x960");
+        resolutions.Add("1920x1080");
+        //resolutions.Add("1024x768, 800x600, 640x480");
+        //resolutions.Add("320x200, 320x240, 800x600");
+        //resolutions.Add("1280x960");
 
-        List<string> expected = new List<string>();
-        expected.Add("(1920, 1080)");
-        expected.Add("(1024, 768)");
-        expected.Add("(800, 600)");
-        expected.Add("(640, 480)");
-        expected.Add("(320, 200)");
-        expected.Add("(320, 240)");
-        expected.Add("(800, 600)");
-        expected.Add("(1280, 960)");
+        IEnumerable<(int, int)> actual = RegExpr.Resolution(resolutions);
+        IEnumerable<(int, int)> expected = new[] {(1920, 1080)};
 
-        Assert.Equal(expected, RegExpr.Resolution(resolution));
+        //, (1024, 768), (800, 600), (640, 480), (320, 200), (320, 240), (800, 600), (1280, 960)
+
+        Assert.Equal(expected, actual);
         
         }
 
